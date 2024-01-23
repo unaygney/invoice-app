645f9b914a589f7c63a8ce2d6930ebbaed92ec9e
Guney Unay <guneymarketing17@hotmail.com>
2024-01-23 18:57:25 +0300

    added color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#7C5DFA",
+        "light-purple": "#9277FF",
+        "very-dark-blue": "#1E2139",
+        "dark-blue": "#252945",
+        "light-gray": "#DFE3FA",
+        "dark-gray": "#888EB0",
+        bluesh: "#7E88C3",
+        "custom-black": "#0C0E16",
+        red: "#EC5757",
+        "light-red": "#FF9797",
+        "light-bg": "#F8F8FB",
+        "custom-black-400": "#141625",
+      },
+    },
   },
   plugins: [],
 };
