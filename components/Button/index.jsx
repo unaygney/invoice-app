d3dafbdafbe117e1e45d9bee2a1d67f8ce9d9c34
Guney Unay <guneymarketing17@hotmail.com>
2024-01-23 18:58:27 +0300

    added button component

---
+++ components/Button/index.jsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import React from "react";
+import style from "./style.module.css";
+import PlusIcon from "./icons/plus.svg";
+
+export default function Button({
+  title,
+  variant,
+  onClick,
+  className,
+  ...rest
+}) {
+  if (variant === "primary") {
+    return (
+      <button
+        onClick={onClick}
+        {...rest}
+        className={clsx(style.button, style.primary, className)}
+      >
+        <PlusIcon />
+        {title}
+      </button>
+    );
+  }
+  return (
+    <button
+      onClick={onClick}
+      {...rest}
+      className={clsx(style.button, style[variant], className)}
+    >
+      {title}
+    </button>
+  );
+}
