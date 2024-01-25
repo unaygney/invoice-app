bca89fa1b49a900bef3cd71de6d69fc58f2a63b5
Guney Unay <guneymarketing17@hotmail.com>
2024-01-25 14:59:04 +0300

    create sidebar component

---
+++ components/Sidebar/index.jsx
@@ -0,0 +1,20 @@
+import { useTheme } from "@/context/Theme/ThemeContext";
+import React from "react";
+export default function SideBar() {
+  const { theme, setTheme } = useTheme();
+  console.log(theme);
+  const handleClick = () => {
+    setTheme(!theme);
+  };
+
+  return (
+    <aside>
+      <button
+        onClick={handleClick}
+        className="bg-gray-600 dark:bg-red rounded-lg w-40 h-10"
+      >
+        change theme{" "}
+      </button>
+    </aside>
+  );
+}
