84a28ad66ea56937365d281986284f30494ab11c
Guney Unay <guneymarketing17@hotmail.com>
2024-01-28 18:16:56 +0300

    create login page

---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginContainer from "@/containers/LoginContainer";
+
+export const metadata = {
+  title: "Login Page",
+  description: "Invoice Web App Login Page ",
+};
+
+export default function Login() {
+  return <LoginContainer />;
+}
