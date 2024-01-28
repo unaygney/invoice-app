147083c9c2f20c0e9481160e00af57cb32109c47
Guney Unay <guneymarketing17@hotmail.com>
2024-01-28 18:18:10 +0300

    create containers file nad added login ,dashboard page

---
+++ containers/DashboardContainer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function DashboardContainer() {
+  return (
+    <main className="bg-light-bg dark:bg-custom-black-400 flex-1  ">test</main>
+  );
+}
---
+++ containers/LoginContainer/LoginForm/index.jsx
@@ -0,0 +1,68 @@
+"use client";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "@/utils/validation";
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => postUserData(data);
+  // post data to api page
+  async function postUserData(data) {
+    try {
+      const response = await fetch("/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const userData = await response.json();
+      return userData;
+      // Burada alınan userData ile yapılacak işlemleri gerçekleştirebilirsiniz.
+    } catch (error) {
+      console.error("Error while posting user data:", error);
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full flex flex-col gap-4 bg-transparent "
+    >
+      <div>
+        <input
+          className="w-full h-12 bg-custom-black-400 px-5 text-gray-300 outline-none"
+          type="email"
+          placeholder="Email address"
+          {...register("email")}
+        />
+        <p className="text-red pt-1 text-xs">{errors.email?.message}</p>
+      </div>
+      <div>
+        <input
+          className="w-full h-12 bg-custom-black-400 px-5 text-gray-300 outline-none"
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        <p className="text-red pt-1 text-xs">{errors.password?.message}</p>
+      </div>
+      <button className="w-full h-12 bg-[#FC4747] text-white text-[18px]  font-bold mt-5 rounded-md hover:bg-white hover:text-[#161D2F] ">
+        Login
+      </button>
+    </form>
+  );
+}
