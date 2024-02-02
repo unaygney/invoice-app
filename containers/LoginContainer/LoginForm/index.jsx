c851c49a9ee6052b89a61f74d78b5086c30496fe
Guney Unay <guneymarketing17@hotmail.com>
2024-02-02 21:16:22 +0300

    added reset func

--- containers/LoginContainer/LoginForm/index.jsx
+++ containers/LoginContainer/LoginForm/index.jsx
@@ -1,23 +1,24 @@
 "use client";
+import { yupResolver } from "@hookform/resolvers/yup";
 import React from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { schema } from "@/utils/validation";
+import { loginSchema } from "@/utils/validation";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function LoginForm() {
+  const REDIRECT_TIME = 1000;
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
+  } = useForm({ resolver: yupResolver(loginSchema) });
 
   const onSubmit = (data) => postUserData(data);
-  // post data to api page
   async function postUserData(data) {
     try {
-      const response = await fetch("/api/user", {
+      const response = await fetch("api/loginUser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -25,14 +26,14 @@ export default function LoginForm() {
         body: JSON.stringify(data),
       });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+      if (response.ok) {
+        toast.success("Successfully logged in!");
+        reset();
+      } else {
+        toast.error(`Error encountered while logging in`);
       }
-
-      const userData = await response.json();
-      return userData;
     } catch (error) {
-      console.error("Error while posting user data:", error);
+      throw new Error("Failed fetching data : ", error);
     }
   }
 
@@ -46,22 +47,23 @@ export default function LoginForm() {
           className="w-full h-12 bg-custom-black-400 px-5 text-gray-300 outline-none"
           type="email"
           placeholder="Email address"
-          {...register("email")}
+          {...register("loginEmail")}
         />
-        <p className="text-red pt-1 text-xs">{errors.email?.message}</p>
+        <p className="text-red pt-1 text-xs">{errors.loginEmail?.message}</p>
       </div>
       <div>
         <input
           className="w-full h-12 bg-custom-black-400 px-5 text-gray-300 outline-none"
           type="password"
           placeholder="Password"
-          {...register("password")}
+          {...register("loginPassword")}
         />
-        <p className="text-red pt-1 text-xs">{errors.password?.message}</p>
+        <p className="text-red pt-1 text-xs">{errors.loginPassword?.message}</p>
       </div>
       <button className="w-full h-12 bg-[#FC4747] text-white text-[18px]  font-bold mt-5 rounded-md hover:bg-white hover:text-[#161D2F] ">
         Login
       </button>
+      <ToastContainer />
     </form>
   );
 }
