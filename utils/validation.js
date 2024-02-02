2ab8aa41137950a7339584bb81a358eb4a2c62c8
Guney Unay <guneymarketing17@hotmail.com>
2024-02-02 21:15:10 +0300

    added new validation schema

--- utils/validation.js
+++ utils/validation.js
@@ -14,3 +14,15 @@ export const schema = yup.object({
     .oneOf([yup.ref("password"), null], "Passwords must match")
     .required("Password confirmation is required"),
 });
+
+export const loginSchema = yup.object({
+  loginEmail: yup
+    .string()
+    .required("Email is required")
+    .email("You entered a invalid email."),
+  loginPassword: yup
+    .string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters long")
+    .max(24, "Password must be max 24characters long"),
+});
