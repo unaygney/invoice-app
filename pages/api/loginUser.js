df28be42f01c8b54bbcbd8ea64a67279650134f9
Guney Unay <guneymarketing17@hotmail.com>
2024-02-05 20:25:40 +0300

    added loginUser info

--- pages/api/loginUser.js
+++ pages/api/loginUser.js
@@ -27,6 +27,7 @@ export default async function handler(req, res) {
     res.status(200).json({
       success: true,
       message: "User data received successfully",
+      user: loginUser,
     });
   } catch (error) {
     console.log("Error Processing user data", error);
