556b639bf106b87649069231e964797f37ad89c3
Guney Unay <guneymarketing17@hotmail.com>
2024-02-08 20:50:44 +0300

    update dashboard/page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,16 +1,6 @@
 import React from "react";
 import DashboardContainer from "@/containers/DashboardContainer";
-import { headers } from "next/headers";
 
 export default async function Dashboard() {
-  const headersList = headers();
-  console.log(headersList);
-
   return <DashboardContainer />;
-  // return (
-  //   <div>
-  //     <h1>Hello</h1>
-  //     <pre>{JSON.stringify(Array.from(headersList.entries()), null, 2)}</pre>
-  //   </div>
-  // );
 }
