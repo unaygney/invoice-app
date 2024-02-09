28f57876ccc863fefefcb85cadc4b396366bd87d
Guney Unay <guneymarketing17@hotmail.com>
2024-02-09 17:59:30 +0300

    create getdata with func

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,10 @@
 import React from "react";
 import DashboardContainer from "@/containers/DashboardContainer";
+import { getDataWithUid } from "@/lib/firebase";
 
 export default async function Dashboard() {
+  const uid = "v3HlQacRvaMUGNAirLC8PXx5LYk1";
+  const data = await getDataWithUid(uid);
+  console.log(data);
   return <DashboardContainer />;
 }
