a3137f77a063edf393ac100e9ab526678095f863
Guney Unay <guneymarketing17@hotmail.com>
2024-02-09 19:26:27 +0300

    getToken function is added

--- utils/helper.js
+++ utils/helper.js
@@ -2,3 +2,7 @@ export const getFirstLetter = (string) => {
   let newStr = string.split("")[0].toUpperCase();
   return newStr;
 };
+
+export const getToken = (str) => {
+  return str.split("=")[1];
+};
