b524e466782ad874e508a041a2bf33277af633f1
Guney Unay <guneymarketing17@hotmail.com>
2024-02-09 20:55:40 +0300

    update dashboard

--- containers/DashboardContainer/index.jsx
+++ containers/DashboardContainer/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import FilterContainer from "./FilterContainer";
+import CardContainer from "./CardContainer";
 
 export default function DashboardContainer({ data }) {
-  console.log(data.invoice);
+  console.log(data.invoice.length);
   return (
-    <main className="bg-light-bg dark:bg-custom-black-400 flex-1  ">
-      'test'
+    <main className="bg-light-bg dark:bg-custom-black-400 text-custom-black-400 dark:text-white flex-1 px-6 py-8  ">
+      <FilterContainer dataLength={data.invoice.length} />
+      <CardContainer data={data.invoice} />
     </main>
   );
 }
