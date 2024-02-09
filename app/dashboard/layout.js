ff274ce90d34944fb56d2ecec12f639bf64596e3
Guney Unay <guneymarketing17@hotmail.com>
2024-02-09 20:56:35 +0300

    minor styles change

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,7 +3,7 @@ import SideBar from "@/components/Sidebar";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`flex flex-col lg:flex-row overflow-hidden `}>
+      <body className={`flex flex-col lg:flex-row  `}>
         <SideBar />
         {children}
       </body>
