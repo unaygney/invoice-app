baa53ece71b1ecd6893612f3751982c205cb39b1
Guney Unay <guneymarketing17@hotmail.com>
2024-02-11 15:31:56 +0300

    create id page

---
+++ app/dashboard/[id]/page.js
@@ -0,0 +1,22 @@
+import ViewContainer from "@/containers/ViewContainer";
+import { verifyJwtToken } from "@/lib/auth";
+import { headers } from "next/headers";
+import { getToken } from "@/utils/helper";
+import { getDataWithUid } from "@/lib/firebase";
+import { notFound } from "next/navigation";
+
+export default async function ViewPage({ params }) {
+  const token = await getToken(headers().get("cookie"));
+  const payload = await verifyJwtToken(token);
+  const data = await getDataWithUid(payload.id);
+  const filteredData = await data.invoices.find(
+    (item) => item.id === params.id
+  );
+
+  // if filtered data is empty
+  if (!filteredData) {
+    return notFound();
+  }
+
+  return <ViewContainer params={params.id} data={filteredData} />;
+}
