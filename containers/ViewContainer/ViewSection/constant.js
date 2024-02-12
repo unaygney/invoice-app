b9740a00687aabd9a0ed9b4e8f71eb342a120808
Guney Unay <guneymarketing17@hotmail.com>
2024-02-12 18:04:03 +0300

    create constant.js and added inputs's array

---
+++ containers/ViewContainer/ViewSection/constant.js
@@ -0,0 +1,89 @@
+export const INPUTS = [
+  {
+    areaName: "Bill From",
+    fields: [
+      {
+        label: "Street Address",
+        name: "street",
+        placeholder: "Enter street address",
+        type: "text",
+      },
+      { label: "City", name: "city", placeholder: "Enter city", type: "text" },
+      {
+        label: "Post Code",
+        name: "postCode",
+        placeholder: "Enter post code",
+        type: "text",
+      },
+      {
+        label: "Country",
+        name: "country",
+        placeholder: "Enter country",
+        type: "text",
+      },
+    ],
+  },
+  {
+    areaName: "Bill To",
+    fields: [
+      {
+        label: "Client's Name",
+        name: "clientName",
+        placeholder: "Enter client's name",
+        type: "text",
+      },
+      {
+        label: "Clients's Email",
+        name: "city",
+        placeholder: "Enter city",
+        type: "email",
+      },
+      {
+        label: "Street Address",
+        name: "clientStreetAddress",
+        placeholder: "Enter Client Street Address",
+        type: "text",
+      },
+      {
+        label: "City",
+        name: "clientCountry",
+        placeholder: "Enter City ",
+        type: "text",
+      },
+      {
+        label: "Post Code",
+        name: "clientPostCode",
+        placeholder: "Enter Post Code ",
+        type: "text",
+      },
+      {
+        label: "Country",
+        name: "clientCountry",
+        placeholder: "Enter Country ",
+        type: "text",
+      },
+    ],
+  },
+  {
+    fields: [
+      {
+        label: "Invoice Date",
+        name: "invoiceDate",
+        placeholder: "Enter invoice date",
+        type: "date",
+      },
+      {
+        label: "Payment Terms",
+        name: "invoiceDate",
+        placeholder: "Enter invoice date",
+        type: "date",
+      },
+      {
+        label: "Project / Description",
+        name: "description",
+        placeholder: "Enter Description date",
+        type: "text",
+      },
+    ],
+  },
+];
