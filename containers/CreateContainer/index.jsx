d2d08ebe55ab56a67a56687edce612758f79d990
Guney Unay <guneymarketing17@hotmail.com>
2024-02-13 21:50:50 +0300

    created createcontainer

---
+++ containers/CreateContainer/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import CreateForm from "./CreateForm";
+import ArrowImg from "@/public/icon-arrow-left.svg";
+import ButtonContainer from "./ButtonContainer";
+function CreateContainer() {
+  return (
+    <main className="bg-light-bg dark:bg-custom-black-400 text-custom-black-400 dark:text-white flex-1 px-6 py-8  ">
+      <div className="max-w-[730px] w-full mx-auto">
+        <div>
+          <Link className="flex  items-center gap-2" href={"/dashboard"}>
+            <ArrowImg /> Go Back
+          </Link>
+          <h2>New Invoice</h2>
+        </div>
+        <CreateForm />
+        <ButtonContainer />
+      </div>
+    </main>
+  );
+}
+
+export default CreateContainer;
