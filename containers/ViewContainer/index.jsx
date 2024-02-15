cf1ed3776ac7f938678c7a326f19266fddec4ab6
Guney Unay <guneymarketing17@hotmail.com>
2024-02-15 18:05:08 +0300

    minor change

--- containers/ViewContainer/index.jsx
+++ containers/ViewContainer/index.jsx
@@ -4,7 +4,7 @@ import ViewSection from "./ViewSection";
 import ButtonContainer from "./ButtonContainer";
 
 export default function ViewContainer({ params, data }) {
-  let strData = JSON.stringify(data);
+  console.log(data);
   return (
     <main className="px-6 py-8 bg-[#f8f8fb] dark:bg-custom-black-400 dark:text-white flex-1 ">
       <div className="max-w-[730px] w-full mx-auto">
