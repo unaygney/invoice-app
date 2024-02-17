ec056995fdb5584b1ed490b908fc856593de47f1
Guney Unay <guneymarketing17@hotmail.com>
2024-02-17 19:25:58 +0300

    added props button container

--- containers/ViewContainer/index.jsx
+++ containers/ViewContainer/index.jsx
@@ -4,13 +4,12 @@ import ViewSection from "./ViewSection";
 import ButtonContainer from "./ButtonContainer";
 
 export default function ViewContainer({ params, data }) {
-  console.log(data);
   return (
     <main className="px-6 py-8 bg-[#f8f8fb] dark:bg-custom-black-400 dark:text-white flex-1 ">
       <div className="max-w-[730px] w-full mx-auto">
         <NavigationBar />
         <ViewSection data={data} />
-        <ButtonContainer />
+        <ButtonContainer params={params} />
       </div>
     </main>
   );
