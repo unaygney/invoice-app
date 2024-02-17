db668c8a4ce832629a3ad25fe75e48dd1d43f83d
Guney Unay <guneymarketing17@hotmail.com>
2024-02-17 19:26:14 +0300

    update button container

--- containers/ViewContainer/ButtonContainer/index.jsx
+++ containers/ViewContainer/ButtonContainer/index.jsx
@@ -1,33 +1,25 @@
 "use client";
 import React from "react";
 import Button from "@/components/Button";
-import { getUserData } from "@/context/AuthContext";
+function ButtonContainer({ params }) {
+  const handleDelete = () => {
+    const response = fetch("/api/deleteData", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    });
 
-function ButtonContainer() {
-  const { setEdit, isEdit } = getUserData();
-  const handleEditClick = () => {
-    setEdit(true);
+    if (response.ok) {
+      console.log("data silindi");
+    }
   };
-
   return (
     <div className="fixed bottom-0 left-0 right-0 h-[91px] bg-white dark:bg-[#1E2139]">
       <div className="w-full h-full flex items-center justify-center gap-2 px-6">
-        {isEdit ? (
-          <div className="w-full h-full flex items-center justify-end gap-2 px-6">
-            <Button variant="dark-mini" title="Cancel" />
-            <Button variant="secondary" title="Save Changes" />
-          </div>
-        ) : (
-          <div className="w-full h-full flex items-center justify-center gap-2 px-6">
-            <Button
-              variant="dark-mini"
-              title="Edit"
-              onClick={handleEditClick}
-            />
-            <Button variant="danger" title="Delete" />
-            <Button variant="secondary" title="Mark as Paid" />
-          </div>
-        )}
+        <div className="w-full h-full flex items-center justify-center gap-2 px-6">
+          <Button variant="danger" title="Delete" onClick={handleDelete} />
+          <Button variant="secondary" title="Mark as Paid" />
+        </div>
       </div>
     </div>
   );
