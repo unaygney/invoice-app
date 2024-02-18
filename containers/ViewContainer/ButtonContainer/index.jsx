7974fd5b01f1cd61c1c391ed36213f1f8a0dee77
Guney Unay <guneymarketing17@hotmail.com>
2024-02-18 16:50:07 +0300

    added button container onclick events

--- containers/ViewContainer/ButtonContainer/index.jsx
+++ containers/ViewContainer/ButtonContainer/index.jsx
@@ -1,24 +1,65 @@
 "use client";
 import React from "react";
 import Button from "@/components/Button";
-function ButtonContainer({ params }) {
-  const handleDelete = () => {
-    const response = fetch("/api/deleteData", {
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+function ButtonContainer({ params, data }) {
+  const router = useRouter();
+  const REDIRECT_TIME = 1000;
+  const status = data.status;
+
+  // * To delete invoice
+  const handleDelete = async () => {
+    const response = await fetch("/api/deleteData", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(params),
     });
 
     if (response.ok) {
-      console.log("data silindi");
+      toast.success("Deleted invoice successfully!");
+      setTimeout(() => {
+        router.push("/dashboard");
+      }, REDIRECT_TIME);
+    } else {
+      toast.error("Encountered a error while deleting data!");
     }
   };
+
+  // * To Handle Update status 'Paid'
+  const handleUpdate = async () => {
+    const response = await fetch("/api/dataUpdate", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(params),
+    });
+
+    if (response.ok) {
+      toast.success("Invoice status changed successfully.");
+      setTimeout(() => {
+        router.push("/dashboard");
+      }, REDIRECT_TIME);
+    } else {
+      toast.error("Encounted a error while update invoice status.");
+    }
+  };
+
   return (
     <div className="fixed bottom-0 left-0 right-0 h-[91px] bg-white dark:bg-[#1E2139]">
       <div className="w-full h-full flex items-center justify-center gap-2 px-6">
         <div className="w-full h-full flex items-center justify-center gap-2 px-6">
-          <Button variant="danger" title="Delete" onClick={handleDelete} />
-          <Button variant="secondary" title="Mark as Paid" />
+          {status !== "paid" ? (
+            <>
+              <Button variant="danger" title="Delete" onClick={handleDelete} />
+              <Button
+                variant="secondary"
+                title="Mark as Paid"
+                onClick={handleUpdate}
+              />
+            </>
+          ) : (
+            <Button variant="danger" title="Delete" onClick={handleDelete} />
+          )}
         </div>
       </div>
     </div>
