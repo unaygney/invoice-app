845f919518111a67ada692525db5980830699a7c
Guney Unay <guneymarketing17@hotmail.com>
2024-02-19 18:35:47 +0300

    token checks id page

--- app/dashboard/[id]/page.js
+++ app/dashboard/[id]/page.js
@@ -3,15 +3,19 @@ import { verifyJwtToken } from "@/lib/auth";
 import { headers } from "next/headers";
 import { getToken } from "@/utils/helper";
 import { getDataWithUid } from "@/lib/firebase";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 export default async function ViewPage({ params }) {
   const token = await getToken(headers().get("cookie"));
   const payload = await verifyJwtToken(token);
-  const data = await getDataWithUid(payload.id);
+  const data = await getDataWithUid(payload?.id);
   const filteredData = await data.invoices.find(
     (item) => item.id === params.id
   );
+
+  if (!token || !payload || !data) {
+    redirect("/login");
+  }
 
   // if filtered data is empty
   if (!filteredData) {
