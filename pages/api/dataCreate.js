8962b700be9d214ca75e00a9b6b65ef7f40bc929
Guney Unay <guneymarketing17@hotmail.com>
2024-02-19 23:55:58 +0300

    minor bug fixed

--- pages/api/dataCreate.js
+++ pages/api/dataCreate.js
@@ -3,19 +3,15 @@ import { verifyJwtToken } from "@/lib/auth";
 import { addInvoiceToUserDoc } from "@/lib/firebase";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const token = getToken(req.headers.cookie);
-    const { id } = await verifyJwtToken(token);
+  const token = getToken(req.headers.cookie);
+  const { id } = await verifyJwtToken(token);
 
-    if (!token && !id) {
-      res.status(401).json({ message: "Unauthorized : Invalid Token or UID!" });
-    }
-    //? is all data keys has a key proporties?
-    if (checkEmptyFields(req.body)) {
-      await addInvoiceToUserDoc(id, req.body);
-      res.status(200).json({ message: "Data created successfully!" });
-    }
+  if (!token && !id) {
+    res.status(401).json({ message: "Unauthorized : Invalid Token or UID!" });
   }
-  // * only accepted post method
-  res.status(400).json({ message: "only accepted post method" });
+  //? is all data keys has a key proporties?
+  if (checkEmptyFields(req.body)) {
+    await addInvoiceToUserDoc(id, req.body);
+    res.status(200).json({ message: "Data created successfully!" });
+  }
 }
