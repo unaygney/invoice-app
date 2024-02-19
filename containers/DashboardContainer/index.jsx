bd87127521cdff8d9145225748c0df1a22cde146
Guney Unay <guneymarketing17@hotmail.com>
2024-02-19 18:35:14 +0300

    stayles change

--- containers/DashboardContainer/index.jsx
+++ containers/DashboardContainer/index.jsx
@@ -5,7 +5,7 @@ import CardContainer from "./CardContainer";
 export default function DashboardContainer({ data }) {
   const invoices = data.invoices;
   return (
-    <main className="bg-light-bg dark:bg-custom-black-400 text-custom-black-400 dark:text-white flex-1 px-6 py-8  ">
+    <main className="bg-light-bg dark:bg-custom-black-400 text-custom-black-400 dark:text-white flex-1 px-6 py-8  overflow-scroll   ">
       <div className="max-w-[730px] w-full mx-auto">
         <FilterContainer dataLength={invoices.length} />
         <CardContainer data={invoices} />
