79f7e690abd99dd6c6d9b324f06003699b84ca51
Guney Unay <guneymarketing17@hotmail.com>
2024-02-19 19:15:19 +0300

    update dashboard container

--- containers/DashboardContainer/index.jsx
+++ containers/DashboardContainer/index.jsx
@@ -1,14 +1,18 @@
+"use client";
 import React from "react";
 import FilterContainer from "./FilterContainer";
 import CardContainer from "./CardContainer";
+import { getUserData } from "@/context/AuthContext";
 
 export default function DashboardContainer({ data }) {
+  const { userData } = getUserData();
   const invoices = data.invoices;
+
   return (
     <main className="bg-light-bg dark:bg-custom-black-400 text-custom-black-400 dark:text-white flex-1 px-6 py-8  overflow-scroll   ">
       <div className="max-w-[730px] w-full mx-auto">
-        <FilterContainer dataLength={invoices.length} />
-        <CardContainer data={invoices} />
+        <FilterContainer data={invoices} />
+        <CardContainer data={userData.length > 0 ? userData : invoices} />
       </div>
     </main>
   );
