49d04aeeaebc210f4ba7b12a02c04ee09027531a
Guney Unay <guneymarketing17@hotmail.com>
2024-02-19 18:34:54 +0300

    props params in viewseciton

--- containers/ViewContainer/index.jsx
+++ containers/ViewContainer/index.jsx
@@ -5,10 +5,10 @@ import ButtonContainer from "./ButtonContainer";
 
 export default function ViewContainer({ params, data }) {
   return (
-    <main className="px-6 py-8 bg-[#f8f8fb] dark:bg-custom-black-400 dark:text-white flex-1 ">
+    <main className="px-6 py-8 bg-[#f8f8fb] dark:bg-custom-black-400 dark:text-white flex-1 overflow-scroll ">
       <div className="max-w-[730px] w-full mx-auto">
         <NavigationBar />
-        <ViewSection data={data} />
+        <ViewSection data={data} params={params} />
         <ButtonContainer params={params} data={data} />
       </div>
     </main>
